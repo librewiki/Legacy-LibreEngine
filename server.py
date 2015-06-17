c086ec0c23cf3d822d4ae25505e31859fc258382
dylanchoi <nayutakorea@gmail.com>
2015-06-17 10:18:48 -0400

    디비 프린트가 안되능군

--- server.py
+++ server.py
@@ -2,16 +2,26 @@ __author__ = 'seyriz'
 
 from flask import *
 from conf.config import *
-from conf.database import *
-from lib.dbConnect import *
+import pymysql
 
-conf = default_config
+conn = pymysql.connect(host='sv3.al.gl', port=3306, user='libredev', passwd='libre_dev', db='libredev', charset='utf8')
+cur = conn.cursor()
+
+
+def printDATABASES():
+	cur.execute("SHOW DATABASES;")
+
+	for row in cur:
+		print(row[0])
+
 
 app = Flask(__name__)
+conf = default_config
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return printDATABASES
+
 
 if(__name__ == "__main__"):
     app.run(conf.HOST,conf.PORT)
