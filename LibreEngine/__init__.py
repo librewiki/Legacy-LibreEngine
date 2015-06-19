8090b55ad84442ef6013c16b2dedf9201d53e166
iTsurea <admin@itsurea.net>
2015-06-19 16:32:05 +0900

    pull용 커밋 ㅠ

--- LibreEngine/__init__.py
+++ LibreEngine/__init__.py
@@ -1,8 +1,6 @@
 __author__ = 'Wonwoo'
 from flask import Flask
-from LibreEngine.conf import config
 
 app = Flask(__name__)
-app.config['NAME'] = config.default_config.NAME
 
 from LibreEngine.include import routes
--- LibreEngine/include/routes.py
+++ LibreEngine/include/routes.py
@@ -1,6 +1,9 @@
 __author__ = '이츠레아'
 from LibreEngine import app
 from flask import render_template, request, flash, session, url_for, redirect
+from LibreEngine.conf import config
+
+app.config['NAME'] = config.default_config.NAME
 
 @app.route('/')
 def index():
