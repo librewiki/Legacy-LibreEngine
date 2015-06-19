c645218c3953447bab946ea9b0996729da313c50
dylanchoi <nayutakorea@gmail.com>
2015-06-19 00:25:07 -0400

    db text 불러오기 설정 끝

--- LibreEngine/lib/dbConnect.py
+++ LibreEngine/lib/dbConnect.py
@@ -1,8 +1,11 @@
-__author__ = '이츠레아'
+__author__ = '이츠레아, 나유타'
+
+
+
 from flask.ext.sqlalchemy import SQLAlchemy
 from LibreEngine import app
 from LibreEngine.conf.database import db_config
 
-app.config["SQLALCHEMY_DATABASE_URI"] = db_config.KIND + "://" + db_config.USER + ":" + db_config.PASS + "@" + db_config.HOST + "/" + db_config.DATABASE
+app.config["SQLALCHEMY_DATABASE_URI"] = db_config.KIND + "://" + db_config.USER + ":" + db_config.PASS + "@" + db_config.HOST + "/" + db_config.DATABASE + "?charset=utf8"
 db = SQLAlchemy()
 db.init_app(app)
