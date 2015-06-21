f12396e9a50f86a2f419c75f37b4a89b844d6bf9
iTsurea <admin@itsurea.net>
2015-06-21 20:55:28 +0900

    불러오기 완료. 없는 페이지에서 로딩이 왜이리김!

--- LibreEngine/include/routes.py
+++ LibreEngine/include/routes.py
@@ -4,20 +4,15 @@ from LibreEngine.include.models import *
 
 @app.route('/')
 def index():
-    return redirect(url_for('wiki/1'))
-
-@app.route('/wiki')
-def next():
-    return redirect(url_for('wiki/1'))
+    return redirect(url_for('read', name='frontpage'))
 
 @app.route('/wiki/<name>')
 def read(name):
     page = WikiText.query.filter(WikiText.document == name).first()
-    return page.text
     if page:
         return render_template('wiki/wiki.html', name=name, contents=page.text)
     else:
-        return "No Such Page"
+        return render_template('wiki/wiki.html', name=name, contents='페이지가 없으영')
 
 @app.route('/wikitext/<int:page_num>')
 def wikipage_num(page_num):
