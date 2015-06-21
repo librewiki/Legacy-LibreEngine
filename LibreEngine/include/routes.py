521cf729e965d58d0c84f464d72d71ab0e804234
iTsurea <admin@itsurea.net>
2015-06-21 20:25:35 +0900

    왜안되닝ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ

--- LibreEngine/include/routes.py
+++ LibreEngine/include/routes.py
@@ -4,11 +4,20 @@ from LibreEngine.include.models import *
 
 @app.route('/')
 def index():
-    return redirect(url_for('wiki'))
+    return redirect(url_for('wiki/1'))
 
 @app.route('/wiki')
-def wiki():
-    return render_template('wiki/wiki.html')
+def next():
+    return redirect(url_for('wiki/1'))
+
+@app.route('/wiki/<name>')
+def read(name):
+    page = WikiText.query.filter(WikiText.document == name).first()
+    return page.text
+    if page:
+        return render_template('wiki/wiki.html', name=name, contents=page.text)
+    else:
+        return "No Such Page"
 
 @app.route('/wikitext/<int:page_num>')
 def wikipage_num(page_num):
